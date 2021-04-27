77e0c9ca009900dd9f876750d30b32dd5f187449
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2021-04-27 17:10:13 +0400

    gotta complete the block-pool add() method

--- classes/block_pool.js
+++ classes/block_pool.js
@@ -5,39 +5,40 @@
  * 4. inform other transaction pools
  * 5. if an address is updated, send the update to others too
  */
-import Block from './block';
+import Block from "./block";
 class Block_pool {
-    constructor() {
-        this.addresses = [];
-        this.pool = [];
-    }
-    set pool(pool)
-    {
-        this.pool = pool;
-    }
-    set addresses(addresses)
-    {
-        // addresses are not supposed to be erased
-        // find a way to update addresses or add new ones
-        this.addresses = addresses;
-    }
-    clear() {}
-    add({pool, addresses}) {
-        // first, verify the hashing. Dump those that dont match hashing if money is present
-        this.pool = pool.map(block => Block.is_valid(block));
-        // then, if it is a receive block, check for a corresponding send block
-        
-        // if the send block exists but something doesnt match, dont update address and delete the receive block
-        // look at each block and search for the corresponding sender address
-        // if it exists
-        // // 1. update the corresponding address state
-        // // 2. sign on it
-        // // 3. send it to others
-        // if an address has new timestamp  
-    }
-    return_existing() {}
-    return_valid() {}
-    set_map() {}
-    clear_if_acepted() {}
+  constructor() {
+    this.addresses = [];
+    this.pool = [];
+  }
+  set pool(pool) {
+    this.pool = pool;
+  }
+  set addresses(addresses) {
+    // addresses are not supposed to be erased
+    // find a way to update addresses or add new ones
+    this.addresses = addresses;
+  }
+  clear() {}
+  add({ pool, addresses }) {
+    // first, preliminary checking
+    this.pool = pool.map((block) => Block.is_valid(block));
+
+    // then, if it is a receive block, check for a corresponding send block
+    // remove both if both match
+    // remove just the receive if they dont match
+    // look at each block and search for the corresponding sender address
+    // if it exists
+    // // 1. update the corresponding address state
+    // // 2. sign on it
+    // // 3. send it to others
+    this.pool = this.pool.map((block) => {})
+    
+    // if an address has new timestamp
+  }
+  return_existing() {}
+  return_valid() {}
+  set_map() {}
+  clear_if_acepted() {}
 }
 module.expors = Block_pool;
